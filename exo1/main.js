7427f4907fdf5129b940907de019f41d2a6eeaed
soilihi ambdouroihmane <soilihiambdouroihmane@gmail.com>
2024-07-03 11:27:11 +0000

    update

---
+++ exo1/main.js
@@ -0,0 +1,15 @@
+// Comment déclarer un tableau en JS
+
+let tableau = ["String",12];
+ 
+// Ajouter une valeur à la fin du tableau
+tableau.push("New value")
+
+// Afficher le tableau dans le console
+console.log(tableau);
+
+// Suprimer la dernier valeur du tableau
+tableau.pop();
+
+// Afficher les tableau dans le console
+console.log(tableau);
